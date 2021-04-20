8b74a9ae2f6b7463e933590467db6dff06be73c2
Mukul1998PS <mukulshishodia2020@gmail.com>
2021-04-20 09:53:08 +0530

    commit

---
+++ src/com/sapient/trg/pl/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sapient.trg.pl;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+System.out.println("Today is:"+new Date());
+	}
+
+}
